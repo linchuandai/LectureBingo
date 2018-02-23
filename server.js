11e91b4746269ee6d65bf49b323d09e8059622ad
Leon Dai <linchuandai@gmail.com>
2018-02-22 23:14:13 -0500

    updated game board

--- server.js
+++ server.js
@@ -4,5 +4,11 @@ var http = require('http').Server(app)
 
 app.use(express.static(__dirname))
 
+var array = [];
+
+for (var i = 0; i < 25; i++){
+    array[i] = i+1;
+}
+
 var server = http.listen(3000, () => {
 })
